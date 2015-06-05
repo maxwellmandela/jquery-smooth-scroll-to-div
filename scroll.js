cca7f9f0d415fed3543a82c9c5552bd4a884a66c
maxwellmandela <maxywhyner94@gmail.com>
2015-06-05 11:39:09 +0300

    Make div full page onscroll

--- scroll.js
+++ scroll.js
@@ -3,14 +3,13 @@
 * Author : Maxwell Mandela
 */
 $('a[href^="#"]').on('click', function(event) {
-
     var target = $( $(this).attr('href') );
-
     if( target.length ) {
         event.preventDefault();
         $('html, body').animate({
             scrollTop: target.offset().top
         }, 1000);
+        $(target).css("height", $(document).height());
     }
 
 });
